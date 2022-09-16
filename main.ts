62774817df49c2e5fe5804eabdcf37779e86d085
Ewen Le Bihan <hey@ewen.works>
2022-09-16 11:06:17 +0200

    🎉 Initial commit

--- main.ts
+++ main.ts
@@ -1,137 +1,321 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { readdir, readFile, readFileSync } from "fs"
+import expandHomeDir from "expand-home-dir"
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian"
+import * as exp from "constants"
+import * as path from "path"
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface UltiSnipsSettings {
+	snippetsDir: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+type SnippetOptions = ("r" | "w" | "i" | "b" | "A" | "t" | "m")[]
+
+class SnippetDefinition {
+	priority: number
+	trigger: string
+	value: string
+	description: string
+	options: string
+	globals: { [k: string]: string[] }
+	location: string
+	context: string
+	actions: { [k: string]: string }
+	matched: boolean
+	lastRegex: null | RegExp
+
+	constructor(
+		priority: number,
+		trigger: string,
+		value: string,
+		description: string,
+		options: string,
+		globals: { [k: string]: string[] },
+		location: string,
+		context: string,
+		actions: { [k: string]: string }
+	) {
+		this.priority = priority
+		this.trigger = trigger
+		this.value = value
+		this.description = description
+		this.options = options
+		this.globals = globals
+		this.location = location
+		this.context = context
+		this.actions = actions
+		// TODO
+	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface Snippet {
+	priority: number
+	trigger: string
+	value: string
+	description: string
+	options: SnippetOptions
+	globals: { [global: string]: string }
+	location: string
+	context: string
+	actions: { [action: string]: string }
+}
+
+const DEFAULT_SETTINGS: UltiSnipsSettings = {
+	snippetsDir: "~/.config/nvim/UltiSnips/",
+}
+
+export default class UltiSnips extends Plugin {
+	settings: UltiSnipsSettings
+	snippets: { [language: string]: Snippet[] }
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		await this.loadSettings()
+		await this.loadSnippets()
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new UltiSnipsSettingTab(this.app, this))
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		)
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		await this.saveData(this.settings)
+	}
+
+	async loadSnippets() {
+		let snippetDir = expandHomeDir(this.settings.snippetsDir)
+		console.info(`Loading snippets from ${snippetDir}…`)
+		await readdir(snippetDir, (err, files) => {
+			if (err) throw err
+			let snippetFiles = files.filter(f => f.endsWith(".snippets"))
+			this.snippets = Object.fromEntries(
+				snippetFiles.map(f => [
+					f.replace(/^(.+)\..+$/, "$1"),
+					[
+						...this.parseSnippets(
+							path.join(snippetDir, f),
+							readFileSync(path.join(snippetDir, f), {
+								encoding: "utf-8",
+							})
+						),
+					],
+				])
+			)
+		})
+		const loadedSnippetsCount = Object.values(this.snippets)
+			.map(s => s.length)
+			.reduce((a, b) => a + b, 0)
+		const loadedSnippetsFiles = Object.keys(this.snippets).length
+		new Notice(
+			`UltiSnips: Loaded ${loadedSnippetsCount} snippet${
+				loadedSnippetsCount === 1 ? "" : "s"
+			} from ${loadedSnippetsFiles} file${
+				loadedSnippetsFiles === 1 ? "" : "s"
+			}`
+		)
+	}
+
+	*parseSnippets(filename: string, raw: string): Iterable<Snippet> {
+		console.info(`Parsing snippets from ${filename}`)
+		let pythonGlobals = null
+		let currentLineNumber = 0
+		let currentPriority = 0
+		let actions = {}
+		let context = null
+
+		function headTail(line: string) {
+			return line.split(" ", 2)
+		}
+
+		function handleSnippetOrGlobal(
+			filename: string,
+			line: string,
+			currentLineNumber: number,
+			lines: string[],
+			pythonGlobals: { [k: string]: string[] },
+			currentPriority: number,
+			preExpand: { [k: string]: string },
+			context: string
+		): ["error", [string, number]] | ["snippet", [SnippetDefinition]] {
+			let description = ""
+			let options = ""
+			const startingLineNumber = currentLineNumber
+
+			let [head, tail] = line.split(" ", 2)
+			let words = tail.split(" ")
+
+			if (words.length > 2) {
+				// second to last word ends with a quote
+				if (
+					!words.at(-1)?.includes('"') &&
+					words.at(-2)?.endsWith('"')
+				) {
+					options = words.at(-1) as string
+					tail = tail
+						.substring(0, tail.length - options.length - 1)
+						.trimEnd()
+				}
+			}
+
+			if (options.includes("e") && !context) {
+				let left = tail.substring(0, tail.length - 1).lastIndexOf('"')
+				if (![-1, 0].includes(left)) {
+					;[context, tail] = [
+						tail.substring(left).replace('"', ""),
+						tail.substring(0, left),
+					]
+				}
+			}
+
+			tail = tail.trim()
+			if (tail.length > 1 && tail.endsWith('"')) {
+				let left = tail.substring(0, tail.length - 1).lastIndexOf('"')
+				if (![-1, 0].includes(left)) {
+					;[description, tail] = [
+						tail.substring(left),
+						tail.substring(0, left),
+					]
+				}
+			}
+
+			let trigger = tail.trim()
+			if (trigger.length > 1 || options.includes("r")) {
+				if (trigger.at(0) != trigger.at(-1)) {
+					return [
+						"error",
+						[
+							`Invalid multiword trigger: '${trigger}'`,
+							currentLineNumber,
+						],
+					]
+				}
+				trigger = trigger.substring(1, trigger.length - 1)
+			}
+
+			let end = "end" + head
+			let content = ""
+
+			let foundEnd = false
+			for (const line of lines) {
+				if (line.trimEnd() == end) {
+					content = content.substring(0, content.length - 1) // chomp last newline
+					foundEnd = true
+					break
+				}
+				content += line
+				currentLineNumber++
+			}
+
+			if (!foundEnd) {
+				return [
+					"error",
+					[`Missing '${end}' for '${trigger}'`, currentLineNumber],
+				]
+			}
+
+			switch (head) {
+				case "global":
+					pythonGlobals[trigger].push(content)
+					break
+
+				case "snippet":
+					return [
+						"snippet",
+						[
+							new SnippetDefinition(
+								currentPriority,
+								trigger,
+								content,
+								description,
+								options,
+								pythonGlobals,
+								`${filename}:${startingLineNumber}`,
+								context,
+								preExpand
+							),
+						],
+					]
+			}
+
+			return [
+				"error",
+				[`Invalid snippet type: '${head}'`, currentLineNumber],
+			]
+		}
+
+		for (const line of raw.split("\n")) {
+			if (!line.trim()) continue
+
+			let [head, tail] = headTail(line)
+			if (["snippet", "global"].includes(head)) {
+				let snippet = handleSnippetOrGlobal(
+					filename,
+					line,
+					currentLineNumber,
+					raw.split("\n"),
+					pythonGlobals,
+					currentPriority,
+					actions,
+					context
+				)
+
+				actions = {}
+				context = null
+				if (snippet !== null) {
+					return ["snippet", [snippet]]
+				}
+			}
+
+			currentLineNumber++
+		}
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class UltiSnipsSettingTab extends PluginSettingTab {
+	plugin: UltiSnips
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	constructor(app: App, plugin: UltiSnips) {
+		super(app, plugin)
+		this.plugin = plugin
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this
 
-		containerEl.empty();
+		containerEl.empty()
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "UltiSnips settings" })
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Snippets folder")
+			.setDesc("Where are your .snippet files?")
+			.addText(text =>
+				text
+					.setPlaceholder("/path/to/my/snippet/files")
+					.setValue(this.plugin.settings.snippetsDir)
+					.onChange(async value => {
+						this.plugin.settings.snippetsDir = value
+						await this.plugin.saveSettings()
+						await this.plugin.loadSnippets()
+					})
+			)
 	}
 }
